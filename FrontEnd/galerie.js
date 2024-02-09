81770bb3db688b22dca71a0e00d3dd3c774bad0b
Julie Lausecker <julielausecker@gmail.com>
2024-02-09 14:55:52 +0100

    Ajout de la galerie via JS

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -1,7 +1,7 @@
-// Récupération des travaux de l'architecte depuis l'API
-const reponse = await fetch("http://localhost:5678/api/works")
-travaux = await reponse.json(); //on convertit la réponse en JSON
+// Récupération des travaux de l'architecte depuis l'API + conversion en json
+const travaux = await fetch("http://localhost:5678/api/works").then(travaux => travaux.json())
 
+console.log(travaux)
 
 // Création de la galerie à partir des données de l'API
 function genererGalerie (travaux)
@@ -15,13 +15,24 @@ function genererGalerie (travaux)
         // Création d'une balise dédiée à chaque projet
         const projetElement = document.createElement("figure")
 
+
         // On créé une image
         const imageElement = document.createElement("img")
         // On désigne la source de l'image à chercher dans le JSON
         imageElement.src = projet.imageUrl
 
-        // On créé la balise pour y mettre le nom de l'image
+        // On créé la balise pour y mettre le titre de l'image
         const nomElement = document.createElement("figcaption")
         nomElement.innerText = projet.title
+
+
+        // On rattache la balise (qui va contenir notre card) à la section galerie (.gallery)
+        sectionGalerie.appendChild(projetElement)
+        // On rattache l'image au projet/notre card
+        projetElement.appendChild(imageElement)
+        // Et on ajoute le texte au projet
+        projetElement.appendChild(nomElement)
     }
 }
+
+genererGalerie(travaux)
